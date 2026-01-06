6ee2594db1ad37ebeb43dd6fee2c068e36b39171
90945 <909458209@qq.com>
2026-01-06 19:49:20 +0800

    init

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const {app, BrowserWindow} = require('electron/main')
 const path = require('node:path')
 
+// ⚠️ 必须在任何 app.on / whenReady 之前
+if (require('electron-squirrel-startup')) {
+  app.quit()
+}
+
 app.whenReady().then(() => {
   const win = new BrowserWindow({
     width: 800,
@@ -9,8 +14,13 @@ app.whenReady().then(() => {
       // __dirname是path.dirname()的简写
       preload: path.join(__dirname, 'preload.js')
     },
+    show: false,
+    backgroundColor: '#ffffff',
   })
   win.loadURL('https://jiangjiang0123.cn/nuxt')
+  win.once('ready-to-show', () => {
+    win.show()
+  })
 })
 
 app.on('window-all-closed', () => {
